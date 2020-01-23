ca081ee3b9d1b7d1d9dcb651a57f9f3d8c42a22d
mireknosek <mirek.nosek@gmail.com>
2020-01-23 10:03:55 +0100

    utworzenie star

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ const client = axios.create({
   baseURL: 'https://api.github.com/',
   responseType: 'json',
   headers: {
-    'Authorization': 'token bbac62ff1821204eeb2a2fb3edff4aad1cad427b'
+    'Authorization': 'token cad204179aaeb0ac865efa9c78e5959d0c9abd62'
   }
 });
 
-//tworzenie gists
-client.post('/gists', {
-  description: "lista",
-  public: true,
-  files: {"name.txt": {content:"Mirek"}}
-})
-  .then(function(response) {
-    console.log('ok')
-  })
-  .catch(function(error) {
-    console.log(erro);
-  })
+//utworzenie star
+  client.put('/gists/8623a2538b342052dc7859501d8176cf/star', {
+    'star': {
+      headers:{
+        'Content-Lenght': 0
+    }}
+    }).then(function(response){
+        console.log("ok")
+      })
+      .catch(function(error){
+        console.log("error")
+      })
